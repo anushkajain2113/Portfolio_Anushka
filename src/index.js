71aaf7532d666ce6fcca88c958de8ce7501992f9
Anushka Jain <medhyajain@gmail.com>
2024-04-13 10:02:01 -0700

    modified after error added openssl-leagacy-provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from './Context';
+import 'swiper/swiper.scss';
 
 ReactDOM.render(
   <ThemeProvider>
